488a995fe9c6438a6bdaa9457d0808641cf5898d
hannguyen10 <baohannguyen864@yahoo.de>
2026-01-15 00:16:46 +0100

    add glow

--- SG_FR_Muenster/Assets/NumberGenerator.cs
+++ SG_FR_Muenster/Assets/NumberGenerator.cs
@@ -5,26 +5,30 @@ using UnityEngine;
 public class NumberGenerator : MonoBehaviour, IInteractable
 {
     private Renderer rend;
-    private Color originalColor;
+    private Material mat;
+
+    private Color baseEmission = Color.black;
+    private Color glowEmission = Color.yellow * 2f; // Intensität!
 
     void Start()
     {
         rend = GetComponent<Renderer>();
-        originalColor = rend.material.color;
+        mat = rend.material; // eigene Instanz!
+        mat.EnableKeyword("_EMISSION");
+    }
+
+    public void OnHoverEnter()
+    {
+        mat.SetColor("_EmissionColor", glowEmission);
+    }
+
+    public void OnHoverExit()
+    {
+        mat.SetColor("_EmissionColor", baseEmission);
     }
 
     public void Interact()
     {
         Debug.Log(Random.Range(0, 100));
-    }
-
-    public void OnHoverEnter()
-    {
-        rend.material.color = Color.yellow; // Leuchten
-    }
-
-    public void OnHoverExit()
-    {
-        rend.material.color = originalColor; // Zurücksetzen
     }
 }
