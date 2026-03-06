1b3e2659e05cfdce89a3722ddea292dac822ad28
hannguyen10 <baohannguyen864@yahoo.de>
2026-03-06 01:51:57 +0100

    add models

--- SG_FR_Muenster/Assets/Scripts/Player/MouseMovement.cs
+++ SG_FR_Muenster/Assets/Scripts/Player/MouseMovement.cs
@@ -12,8 +12,8 @@ public class MouseMovement : MonoBehaviour
     public Camera cam;
     private float xRotation = 0f;
 
-    public float xSensitivity = 25f;
-    public float ySensitivity = 25f;
+    public float xSensitivity = 2f;
+    public float ySensitivity = 2f;
 
     public void ProcessLook(Vector2 input)
     {
